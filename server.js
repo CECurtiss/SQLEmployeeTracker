b29f3141043bb9eaf3924118daf952317315174b
CECurtiss <curtissclinton@gmail.com>
2022-12-11 13:20:06 -0600

    trying to fix connection issues with db

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const mysql = require('mysql2');
-const inquirer = require('express');
-const consoleTable = require('console.table');
 require('dotenv').config();
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
 
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'process.env.DB_USER',
-        password: 'process.env.DB_PASSWORD',
-        database: 'process.env.DB_NAME'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the company_db database.`)
 );
